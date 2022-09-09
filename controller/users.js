4e29437d795a0b731fef0c64f78181eb8ff4384e
David Buckley <davidpaulbuckley@gmail.com>
2022-09-09 13:15:28 +1000

    Added a check to make sure password & confirmed-password match, updated user controller signup route to check for existing username first, then for existing email, and added a bit more css to make the form readable/useable.

--- controller/users.js
+++ controller/users.js
@@ -7,18 +7,18 @@ const router = express.Router();
 // Login User
 router.post('/', (request, response) => {
     const email = request.body.email.toLowerCase()
-    const password = request.body.password;  
+    const password = request.body.password;
     User.checkExists(email)
     .then(dbRes => {
         if (dbRes.rowCount === 0) {
             return response.status(400).json({message: 'The username and/or password you have entered is incorrect.'})
-        } 
+        }
         const user = dbRes.rows[0];
-        const hashedPassword = user.password;        
+        const hashedPassword = user.password;
         if (isValidPassword(password, hashedPassword)) {
             request.session.email = email;
             return response.json({})
-        } 
+        }
         return response.status(400).json({message: 'The username and/or password you have entered is incorrect.'})
     })
     .catch(() => response.sendStatus(500))
@@ -29,9 +29,9 @@ router.delete('/', (request, response) => {
     const userExists = request.session.email
     if (userExists) {
         request.session.destroy()
-        return response.json({})   
+        return response.json({})
     } else {
-        return response.status(400).json({message: 'No users are logged in. How did you get here!?'})   
+        return response.status(400).json({message: 'No users are logged in. How did you get here!?'})
     }
 });
 
@@ -47,11 +47,21 @@ router.get('/', (request, response) => {
 
 // Sign up new user
 router.post('/signup', (request, response) => {
-    const { username, email, password, securityQuestion, securityAnswer } = request.body;
+    const { username, email, confirmedPassword, securityQuestion, securityAnswer } = request.body;
     User.checkDoesNotExist(email,username)
     .then(dbRes => {
+        let usernameExists = false;
+        let emailExists = false;
+        for (let i = 0; i < dbRes.rowCount; i++) {
+            if (dbRes.rows[i]['username'] === username) {
+                usernameExists = true;
+            }
+            if (dbRes.rows[i]['email'] === email) {
+                emailExists = true;
+            }
+        }
         if(dbRes.rowCount === 0) {
-            const hashedPassword = generateHash(password);
+            const hashedPassword = generateHash(confirmedPassword);
             const hashedSecurityAnswer = generateHash(securityAnswer);
             return User.addNewUser(username, email, hashedPassword, securityQuestion, hashedSecurityAnswer)
             .then(dbRes => {
@@ -60,9 +70,9 @@ router.post('/signup', (request, response) => {
             .catch(err => {
                 return response.status(500).json( {message: err} );
             })
-        } else if (dbRes.rows[0]['username'] === username) {
+        } else if (usernameExists === true) {
             return response.status(400).json( {message: 'Username already taken, please choose another.'} )
-        } else if (dbRes.rows[0]['email'] === email) {
+        } else if (emailExists === true) {
             return response.status(400).json( {message: 'Email already registered, please proceed to login.'} )
         } else {
             return response.sendStatus(500);
