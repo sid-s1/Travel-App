ea8c0d9e9514f38da7cef37623c067aee0d46cf6
Chris HS <chris.hollidaysmith@travelpartners.com.au>
2022-09-09 18:46:11 +1000

    Added 'profile_pic' field to the database schema and seed

--- database/seed.sql
+++ database/seed.sql
@@ -4,11 +4,11 @@
 -- Note: passwords for users are same as usernames
 
 TRUNCATE users restart identity cascade;
-INSERT INTO users(username, email, password, security_qn, security_ans, admin) VALUES
-('chris', 'chris@ga.com', '$2b$10$wc..uujGo3b3ceXnN80tzOYaWuPXQKu.0GoEWdc2jKVi47g91xDAi', 1, 'test', 't'),
-('sid', 'sid@ga.com', '$2b$10$dhxz25GjwVgV2YkgJxBK8eWPxoSjOJywSdnRXfbz78WYXvlj7el6S', 2,'test', 't'),
-('dave', 'dave@ga.com', '$2b$10$ng..DJ0BE9VdUvUTk3.dqunsplnc673tHnT9I/g3tY4aD7LgWzCnK', 3,'test', 't'),
-('sam', 'sam@ga.com', '$2b$10$kfi/.jZLiYM6hBWEagFvgeaN/IKSJFFdNuop0IhTbyGHYOMGZxY6G', 4,'test', 'f');
+INSERT INTO users(username, email, password, security_qn, security_ans, profile_pic, admin) VALUES
+('chris', 'chris@ga.com', '$2b$10$wc..uujGo3b3ceXnN80tzOYaWuPXQKu.0GoEWdc2jKVi47g91xDAi', 1, 'test', null, 't'),
+('sid', 'sid@ga.com', '$2b$10$dhxz25GjwVgV2YkgJxBK8eWPxoSjOJywSdnRXfbz78WYXvlj7el6S', 2,'test', null, 't'),
+('dave', 'dave@ga.com', '$2b$10$ng..DJ0BE9VdUvUTk3.dqunsplnc673tHnT9I/g3tY4aD7LgWzCnK', 3,'test', null, 't'),
+('sam', 'sam@ga.com', '$2b$10$kfi/.jZLiYM6hBWEagFvgeaN/IKSJFFdNuop0IhTbyGHYOMGZxY6G', 4,'test', null, 'f');
 
 TRUNCATE trips restart identity cascade;
 INSERT INTO trips(user_id, trip_name, trip_type, trip_status, trip_start_date, trip_end_date, hero_image_url, description, key_takeaway) VALUES
