32e8115b420aea1fe32cfdd25925a57f05d6d900
Chris HS <chris.hollidaysmith@travelpartners.com.au>
2022-09-14 21:53:33 +1000

    autocomplete function refactored to allow for second param to adjust how code is executed

--- views/js/components/autocomplete.js
+++ views/js/components/autocomplete.js
@@ -1,22 +1,9 @@
-export function initAutocompleteHotels(element) {
-    const autocomplete = new google.maps.places.Autocomplete(
-        element, // use the input box query selector where we want autocomplete
-        {
-            types: ['lodging'],
-            // removed 'types' and 'componentRestrictions' keys so as to not restrict users in entering any kind of place/activity
-            fields: ['place_id', 'geometry', 'name'] // see what more we can get
-        }
-    );
-    return autocomplete;
-}
+export function initAutocomplete(element, type) {
+    let data = {};
+    if (type === 'hotel') {
+        data.types = ['lodging']
+    };
+    data.fields = ['place_id', 'geometry', 'name'];
 
-export function initAutocompleteActivities(element) {
-    const autocomplete = new google.maps.places.Autocomplete(
-        element, // use the input box query selector where we want autocomplete
-        {
-            // removed 'types' and 'componentRestrictions' keys so as to not restrict users in entering any kind of place/activity
-            fields: ['place_id', 'geometry', 'name'] // see what more we can get
-        }
-    );
-    return autocomplete;
+    return new google.maps.places.Autocomplete(element, data);
 }
